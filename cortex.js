8170d6f83276208612ef3ebe4f9666b353cc62b5
Michel Brauna <michelbrauna@gmail.com>
2018-02-16 21:47:31 -0200

    .
    .

---
+++ cortex.js
@@ -0,0 +1,40 @@
+/********************************************************************************
+ * Autor: Michel Brauna                                        Data: 16/02/2018 *
+ *                                                                              *
+ *                  ------------------------------------------                  *
+ *             _________                __         ____  ___                    *
+ *             \_   ___ \  ____________/  |_  ____ \   \/  /                    *
+ *             /    \  \/ /  _ \_  __ \   __\/ __ \ \     /                     *
+ *             \     \___(  <_> )  | \/|  | \  ___/ /     \                     *
+ *              \______  /\____/|__|   |__|  \___  >___/\  \                    *
+ *                     \/                        \/      \_/                    *
+ *                                                                              *
+ *                  ------------------------------------------                  *
+ *                                                                              *
+ *   Procedimento inicial responsável pela inicialização de todas as funcionali-*
+ * dades para CórteX. Para toda ação do bot, um evento irá ser executado, quer  *
+ * seja um dependente de informação externa ou não.                             *
+ *   CorteX possuirá uma inteligência artificial para determinar as melhores op-*
+ * ções nas respostas criadas ou quiçá nas buscas.                              *
+ *                                                                              *
+ *                  ------------------------------------------                  *
+ *             _________                __         ____  ___                    *
+ *             \_   ___ \  ____________/  |_  ____ \   \/  /                    *
+ *             /    \  \/ /  _ \_  __ \   __\/ __ \ \     /                     *
+ *             \     \___(  <_> )  | \/|  | \  ___/ /     \                     *
+ *              \______  /\____/|__|   |__|  \___  >___/\  \                    *
+ *                     \/                        \/      \_/                    *
+ *                                                                              *
+ *                  ------------------------------------------                  *
+ *                                                                              *
+ ********************************************************************************/
+
+
+// Inicialização de bibliotecas     (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
+const bib_log = require('CorteX/_Log/geraLog.js');
+// Fim - Inicialização de bibliotecas     (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
+
+
+// Inicialização das variáveis de ambiente （ ^_^）o自  自o（^_^ ）
+
+// Fim - Inicialização das variáveis de ambiente （ ^_^）o自  自o（^_^ ）
