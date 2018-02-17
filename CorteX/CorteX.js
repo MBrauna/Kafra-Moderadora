85dbaa7dad8c3f865cd7181274dfcb0bafdf8c53
Michel Brauna <michelbrauna@gmail.com>
2018-02-17 10:45:16 -0200

    INITIAL COMMIT
    INITIAL COMMIT

--- CorteX/CorteX.js
+++ CorteX/CorteX.js
@@ -11,11 +11,10 @@
  *                                                                              *
  *                  ------------------------------------------                  *
  *                                                                              *
- *   Procedimento inicial responsável pela inicialização de todas as funcionali-*
- * dades para CórteX. Para toda ação do bot, um evento irá ser executado, quer  *
- * seja um dependente de informação externa ou não.                             *
- *   CorteX possuirá uma inteligência artificial para determinar as melhores op-*
- * ções nas respostas criadas ou quiçá nas buscas.                              *
+ *                         CLASSE PRINCIPAL PARA CÓRTEX                         *
+ *  Responsável por "ouvir" as portas necessárias para execução das bibliotecas *
+ *assim como armazenar e deliberadamente através de sua A.I. decidir qual a     *
+ *melhor funcionalidade para aquela ação.                                       *
  *                                                                              *
  *                  ------------------------------------------                  *
  *             _________                __         ____  ___                    *
@@ -31,33 +30,48 @@
 
 
 // Inicialização de bibliotecas                                 (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
-const bib_cortex                  =   require('./CorteX/CorteX.js');
+let bib_discord                     =   require('discord.js')
+   ,bib_telegram                    =   require('telegraf');
 // Fim - Inicialização de bibliotecas                           (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
 
 
+class CorteX
+{
+    // Método construtor      ¯\_(⊙︿⊙)_/¯
+    constructor(p_token_discord
+               ,p_token_telegram
+               )
+    {
+        /************************************************************************
+         * Método responsável por inicializar o CórteX e armazenar virtualmente *
+         * todos os tokens de acesso.                                           *
+         ************************************************************************/
 
-// ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
+        // Salva virtualmente - protected - os dados do token
+        this.v_token_discord        =   p_token_discord;
+        this.v_token_telegram       =   p_token_telegram;
+        // FIM - Salva virtualmente - protected - os dados do token
+
+        // Instancia uma nova sessão para o acesso ao bot
+        this.bib_cortex_discord     =   new bib_discord.Client();
+        this.bib_cortex_telegram    =   new bib_cortex_telegram(this.v_token_telegram);
+        //FIM - Instancia uma nova sessão para o acesso ao bot
+    }   // Método construtor      ¯\_(⊙︿⊙)_/¯
 
 
 
-// Inicialização das variáveis de ambiente                      （ ^_^）o自  自o（^_^ ）
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
 
-// Tokens
-let v_token_discord             =   process.env.BOT_TOKEN_DISCORD
-   ,v_token_telegram            =   process.env.BOT_TOKEN_TELEGRAM
-   ;
-
-// Fim - Inicialização das variáveis de ambiente                （ ^_^）o自  自o（^_^ ）
-
-
-
-// ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
+    inicializa()
+    {
+        /************************************************************************
+         *   Método responsável por inicializar as bibliotecas mediante token e *
+         * conforme eventos pré-definidos disparar trigger.                     *
+         ************************************************************************/
 
 
+    }   // inicializa()
+}   // CLASS CORTEX
 
-// Método construtor - Inicialização do processo                (╬ ಠ益ಠ)
-const init_CorteX                 =   bib_cortex(v_token_discord
-                                                ,v_token_telegram
-                                                )
-                                                .inicializa();
-// FIM - Método construtor - Inicialização do processo          (╬ ಠ益ಠ)
+// Torna o método público
+module.exports = bropediaBot;
