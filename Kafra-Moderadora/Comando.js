5b0020ef916e2652afa2f16b9b0b42add4916e4e
Michel Brauna <michelbrauna@gmail.com>
2018-02-21 15:37:24 -0300

    Update Comando.js

--- Kafra-Moderadora/Comando.js
+++ Kafra-Moderadora/Comando.js
@@ -43,41 +43,7 @@ class comando
 
             console.log(v_tmp_prefixo);
             console.log(v_tmp_tamanho);
-            console.log(this.init_kafra.prefixo.length);
-
-            
-            // Verifica se a mensagem foi iniciada com o prefixo desejado!
-            if(v_tmp_prefixo)
-            {
-                console.log('Provavel comando');
-                console.log(p_mensagem.content.slice(this.init_kafra_prefixo.length).trim().split(/ +/g));
-                console.log('------------------');
-            } // if(v_tmp_prefixo)
-            else if(v_tmp_tamanho > 0)
-            {
-                for(var tmp_integer = 0; tmp_integer < v_tmp_tamanho; tmp_integer++)
-                {
-                    if(p_mensagem.mentions[tmp_integer].id === this.init_kafra.id)
-                    {
-                        // Marca que ocorreu uma menção do bot
-                        console.log(p_mensagem.mentions[tmp_integer].id);
-                        v_tmp_mencao = true;
-                    } // if(p_mensagem.mentions[tmp_integer].id === this.init_kafra.id)
-                } // for(var tmp_integer = 0; tmp_integer < v_tmp_tamanho; tmp_integer++)
-
-                // Caso tenha encontrado uma menção gera alerta
-                if(v_tmp_mencao)
-                {
-                    console.log('Menção');
-                }
-            } // else if(v_tmp_tamanho > 0)
-
-            console.log(p_mensagem);
-        //} // try {}
-        //catch(p_erro)
-        //{
-        //    console.trace();
-        //} // catch(p_erro) {}
+            console.log(this.init_kafra_prefixo.length);
     } // trata_mensagem(p_cliente, p_mensagem)
 } // CLASS COMANDO
 
