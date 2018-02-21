661653bca5f2d53a8628c547ca15c223e1c0546f
Michel Brauna <michelbrauna@gmail.com>
2018-02-21 10:33:38 -0300

    Teste de mensageria
    Teste de mensageria

--- Kafra-Moderadora/kafra-moderadora.js
+++ Kafra-Moderadora/kafra-moderadora.js
@@ -101,34 +101,54 @@ class Kafra_moderadora
          *  >> >>  D I S C O R D   << << *
          *********************************/
         // PARA EVENTO DO TIPO - BOT PREPARADO
-        this.init_kafra_moderadora.on("ready",         () =>
-                                                    {
-                                                        console.log('teste');
-                                                    }
-                                );
+        this.init_kafra_moderadora.on(
+                                        "ready"
+                                       ,v_acesso =>
+                                        {
+                                            console.log('-- -- -- READY -- -- --');
+                                            console.log('---------------------------');
+                                            console.log(v_acesso);
+                                            console.log('---------------------------');
+                                        }
+                                    );
         // PARA EVENTO DO TIPO - BOT PREPARADO
 
         // PARA EVENTO DO TIPO - BOT ENTROU NO CANAL
-        this.init_kafra_moderadora.on("guildCreate",   grupo =>
-                                                    {
-                                                        console.log('teste');
-                                                    }
-                                );
+        this.init_kafra_moderadora.on(
+                                        "guildCreate"
+                                       ,grupo =>
+                                        {
+                                            console.log('-- -- -- GUILD CREATE -- -- --');
+                                            console.log('---------------------------');
+                                            console.log(grupo);
+                                            console.log('---------------------------');
+                                        }
+                                    );
         // PARA EVENTO DO TIPO - BOT ENTROU NO CANAL
 
         // PARA EVENTO DO TIPO - BOT SAIU DO CANAL
-        this.init_kafra_moderadora.on("guildDelete",   grupo =>
-                                                    {
-                                                        console.log('teste');
-                                                    }
-                                );
+        this.init_kafra_moderadora.on(
+                                        "guildDelete"
+                                       ,grupo =>
+                                        {
+                                            console.log('-- -- -- GUILD DELETE -- -- --');
+                                            console.log('---------------------------');
+                                            console.log(grupo);
+                                            console.log('---------------------------');
+                                        }
+                                    );
         // PARA EVENTO DO TIPO - BOT SAIU DO CANAL
 
         // PARA EVENTO DO TIPO TEXTO
-        this.init_kafra_moderadora.on("message",       async mensagem =>
-                                                    {
-                                                        console.log('teste');
-                                                    }
+        this.init_kafra_moderadora.on(
+                                        "message"
+                                       ,async mensagem =>
+                                        {
+                                            console.log('-- -- -- MENSAGEM -- -- --');
+                                            console.log('---------------------------');
+                                            console.log(mensagem);
+                                            console.log('---------------------------');
+                                        }
                                 );
         // PARA EVENTO DO TIPO TEXTO
 
