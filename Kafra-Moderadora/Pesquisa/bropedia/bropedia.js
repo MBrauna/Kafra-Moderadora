a0ada3ad210a6ed8b341db6a1a287bd2fad8e7fc
Michel Brauna <michelbrauna@gmail.com>
2018-02-23 21:26:27 -0300

    Debug
    Debug

--- Kafra-Moderadora/Pesquisa/bropedia/bropedia.js
+++ Kafra-Moderadora/Pesquisa/bropedia/bropedia.js
@@ -25,158 +25,39 @@ let bib_requisicao      =   require('request')
    ;
 // Inicialização de bibliotecas                                 (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
 
+
+// Método construtor - Inicialização do processo                (╬ ಠ益ಠ)
+// Método construtor - Inicialização do processo                (╬ ಠ益ಠ)
+
 class bropedia
 {
     construct()
     {
         console.log('[BROPEDIA] - BIBLIOTECA CARREGADA');
+
+        this.v_url_bropedia
     } // construct()
 
     consultar(p_consulta, p_obj_msg, p_config)
     {
-        // Guarda a string de consulta à enciclopédia
-        let     v_termo_consulta    =   encodeURI(p_consulta.trim())
-               ,v_url_bropedia      =   `http://bropedia.net/api.php?action=query&list=search&srsearch=${v_termo_consulta}&utf8=&format=json`
-               ;
 
-
-        // Inicia o procedimento de consulta ao termo desejado
-        bib_requisicao.get(v_url_bropedia, (p_erro, p_resposta, p_corpo) =>
+        try
         {
-            // Cria as variáveis para tratamento da resposta obtida da Wiki
-            let v_resposta  =   JSON.parse(p_corpo)
-               ,v_pagina;
+            // Prepara os dados iniciais para consulta na enciclopédia.
+            let    v_termo_consulta     =   encodeURI(p_consulta.trim())
+                  ,v_url_bropedia       =   `http://bropedia.net/api.php?action=query&list=search&srsearch=${v_termo_consulta}&utf8=&format=json`
+                  ;
 
-            // Verifica se o resultado da consulta trouxe algum argumento
-            if(v_resposta.query.searchinfo.totalhits == 0)
-            {
-                // Não há informações para o tipo de consulta escolhida
-                p_obj_msg.embed.color           =   p_config.cor_vermelha.color;
-                p_obj_msg.embed.title           =   'TERMO NÃO ENCONTRADO NA WIKI';
-                p_obj_msg.embed.description     =   'Desculpe ): O termo procurado não foi encontrado';
-                p_obj_msg.embed.fields          =   {
-                                                        name: 'Desculpe! Não encontrei nada!'
-                                                       ,value: 'O termo ' + p_consulta + 'procurado não foi encontrado em minha base de dados!'
-                                                    };
-
-                return p_obj_msg;
-            } // if(v_resposta.query.searchinfo.totalhits == 0)
-            else
-            {
-                // Verifica se o termo consultado foi encontrado
-                v_resposta.query.search.forEach((p_retorno) =>
-                {
-                    if(p_retorno.title.toLowerCase() == p_consulta.trim().toLowerCase())
-                    {
-                        // Degine para a Página o retorno da consulta
-                        v_pagina    =   p_retorno;
-                    } // if(p_retorno.title.toLowerCase() == p_consulta.trim().toLowerCase())
-                });
-
-                // Verifica se o dado obtido na consulta existe
-                if(typeof v_pagina === 'undefined')
-                {
-                    // Define um novo valor para a página, no caso um que possa ser consultado
-                    v_pagina    =   bib_underline.first(resp.query.search);
-                } // if(typeof v_pagina === 'undefined')
-
-                // Retorna a página desejada nos formatos esperados
-                return this.geraConsulta(p_obj_msg, p_config, v_pagina);
-            } // else { ... }
-
-        }); // v_url_bropedia, (p_erro, p_resposta, p_corpo) =>
+            return p_obj_msg;
+        } // try { ... }
+        catch(p_erro)
+        {
+            console.log('--------------------');
+            console.log('->> ERRO BROPEDIA ');
+            console.log('--------------------');
+        } // catch(p_erro) { ... }
 
     } // consultar(p_consulta, p_obj_msg, p_config)
-
-
-    geraConsulta(p_obj_msg, p_config, p_pagina)
-    {
-        // Monta a string de consulta à Wiki
-        const v_url_bropedia    =   `http://bropedia.net/api.php?action=query&titles=${p_pagina.title}&prop=info|revisions&inprop=url&rvprop=content&format=json`;
-
-        // Realiza uma consulta nos dados da página
-        bib_requisicao.get(v_url_bropedia, (p_erro, p_resposta, p_corpo) =>
-        {
-            // Declaração das variáveis necessárias para composição do objeto
-            let  v_resposta     = JSON.parse(p_corpo)
-                ,v_pagina       = v_resposta.query.pages[Object.keys(v_resposta.query.pages)[0]]
-                ,v_msg_section  = []
-                ,v_obj_pagina
-                ,v_redirect
-                ;
-
-
-
-
-            p_obj_msg.embed.color           =   p_config.cor_verde.color;
-            p_obj_msg.embed.title           =   v_pagina.title;
-            p_obj_msg.embed.url             =   v_pagina.canonicalurl;
-            p_obj_msg.embed.description     =   'yay Consegui encontrar algo para: ' + v_pagina.title;
-            p_obj_msg.embed.fields          =   {
-                                                    name: '>>> <<<'
-                                                   ,value: 'Teste'
-                                                };
-
-
-
-                /*
-            // Verifica se foi possível encontrar a página desejada
-            if(typeof v_pagina == 'undefined')
-            {
-                p_obj_msg.embed.color           =   p_config.cor_vermelha.color;
-                p_obj_msg.embed.title           =   'TERMO NÃO ENCONTRADO NA WIKI';
-                p_obj_msg.embed.description     =   'Desculpe ): O termo procurado não foi encontrado';
-                p_obj_msg.embed.fields          =   {
-                                                        name: 'Desculpe! Não encontrei nada!'
-                                                       ,value: 'O termo ' + p_consulta + 'procurado não foi encontrado em minha base de dados!'
-                                                    };
-            } // if(typeof v_pagina == 'undefined')
-            else
-            {
-                // Monta um objeto contendo as revisões utilizadas
-                v_obj_pagina    =   bib_wtf_wiki.parse(_.first(v_pagina.revisions)['*']);
-
-                if(typeof v_obj_pagina.sections !== 'undefined')
-                {
-                    v_obj_pagina.sections.filter(
-                                                    (section) =>
-                                                    {
-                                                        return section.sentences.length > 0 && !bib_underline.isEmpty(section.title)
-                                                    }
-                                                ).forEach(
-                                                            (section) =>
-                                                            {
-                                                                v_msg_section.push(
-                                                                {
-                                                                    name    :   section.title,
-                                                                    value   :   bib_underline.first(section.sentences).text
-                                                                })
-                                                            }
-                                                        );
-
-                    // Monta o objeto para retorno
-                    p_obj_msg.embed.color           =   p_config.cor_verde.color;
-                    p_obj_msg.embed.title           =   v_pagina.canonicalurl;
-                    p_obj_msg.embed.description     =   'yay Consegui encontrar algo para: ' + p_pagina.title;
-                    p_obj_msg.embed.fields          =   v_msg_section;
-
-                } // if(typeof v_obj_pagina.sections !== 'undefined')
-                else
-                {
-
-                    p_obj_msg.embed.color           =   p_config.cor_vermelha.color;
-                    p_obj_msg.embed.title           =   'TERMO NÃO ENCONTRADO NA WIKI';
-                    p_obj_msg.embed.description     =   'Desculpe ): O termo procurado não foi encontrado';
-                    p_obj_msg.embed.fields          =   {
-                                                            name: 'Desculpe! Não encontrei nada!'
-                                                           ,value: 'O termo ' + p_consulta + 'procurado não foi encontrado em minha base de dados!'
-                                                        };
-                } // else { ... }
-            } // else { ... }*/
-        }); // bib_requisicao.get(v_url_bropedia, (p_erro, p_resposta, p_corpo) =>
-
-        return p_obj_msg;
-    } // geraConsulta(p_obj_msg, p_config, v_pagina)
 } // class bropedia
 
 
