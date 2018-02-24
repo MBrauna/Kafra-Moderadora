4e4659a001a3f270c48a5398a820b3339153def1
Michel Brauna <michelbrauna@gmail.com>
2018-02-24 00:16:55 -0300

    DEBUG
    DEBUG

--- Kafra-Moderadora/Comando.js
+++ Kafra-Moderadora/Comando.js
@@ -127,7 +127,8 @@ class comando
                                 v_string_requisicao = v_string_requisicao + v_obj_mensagem_s_prefixo[i] + ' ';
                             } // for(var i=1;i<=v_obj_mensagem_s_prefixo.length;i++)
 
-                            obj_msg_tmp                     =   init_bropedia().consultar(v_string_requisicao, obj_msg_tmp, this.init_config);
+                            obj_msg_tmp                     =   init_bropedia.consultar(v_string_requisicao, obj_msg_tmp, this.init_config);
+                            console.log(obj_msg_tmp);
                             break;
                         case 'recrutar':
                             obj_msg_tmp.embed.description   =   'Desculpe pessoinha, mas esta funcionalidade não está disponível no momento.';
