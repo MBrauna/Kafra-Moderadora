055adcfbfe4b5faa4b98ee8d57715dbd64fc1ff7
Michel Brauna <michelbrauna@gmail.com>
2018-02-27 00:55:03 -0300

    Correção bROPédia
    Correção bROPédia

--- Kafra-Moderadora/Pesquisa/bropedia/bropedia.js
+++ Kafra-Moderadora/Pesquisa/bropedia/bropedia.js
@@ -222,8 +222,6 @@ class bropedia
 
                             // Chama o mesmo método para encontrar as informações
                             this.consultar(v_redirect);
-
-                            return;
                         } // if(!v_redirecionamento && !bib_underline.isEmpty(v_revisao) && v_revisao['*'].indexOf('#REDIRECIONAMENTO') > -1)
 
                         // Caso a página não tenha sido encontrada
