8cad22b34ecc7db265b94fe24bd249fee3293ea2
Michel Brauna <michelbrauna@gmail.com>
2018-03-04 12:21:20 -0300

    DEBUG - ID_CANAL NULO
    Código do canal que a mensagem foi recebida está nulo.

--- Kafra-Moderadora/Ragnarok/grupo_ragnarok.js
+++ Kafra-Moderadora/Ragnarok/grupo_ragnarok.js
@@ -194,7 +194,7 @@ class grupo_ragnarok
                 v_mapa      =   p_regra[3].trim();
             } // else { ... }
 
-
+            console.log(this.obj_mensagem);
             v_resultado     = this.obj_database.monta_grupo(this.obj_mensagem.author.id, this.obj_mensagem.channl_id, v_nivel_inicial, v_nivel_final, v_mapa);
 
             if(v_resultado === 1)
