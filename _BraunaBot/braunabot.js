56bdc928a22964ec17eacaa8bfdb2abe8f213930
Michel Brauna <michelbrauna@gmail.com>
2018-03-05 17:45:58 -0300

    Criação do método BraunaBot
    BraunaBot para conversação.

---
+++ _BraunaBot/braunabot.js
@@ -0,0 +1,84 @@
+/****************************************************************************************************
+ * Autor: Michel Brauna                                                            Data: 05/03/2018 *
+ ****************************************************************************************************
+ *                                                                                                  *
+ *                             ██╗  ██╗ █████╗ ███████╗██████╗  █████╗                              *
+ *                             ██║ ██╔╝██╔══██╗██╔════╝██╔══██╗██╔══██╗                             *
+ *                             █████╔╝ ███████║█████╗  ██████╔╝███████║                             *
+ *                             ██╔═██╗ ██╔══██║██╔══╝  ██╔══██╗██╔══██║                             *
+ *                             ██║  ██╗██║  ██║██║     ██║  ██║██║  ██║                             *
+ *                             ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝                             *
+ *                                                                                                  *
+ *       ███╗   ███╗ ██████╗ ██████╗ ███████╗██████╗  █████╗ ██████╗  ██████╗ ██████╗  █████╗       *
+ *       ████╗ ████║██╔═══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔═══██╗██╔══██╗██╔══██╗      *
+ *       ██╔████╔██║██║   ██║██║  ██║█████╗  ██████╔╝███████║██║  ██║██║   ██║██████╔╝███████║      *
+ *       ██║╚██╔╝██║██║   ██║██║  ██║██╔══╝  ██╔══██╗██╔══██║██║  ██║██║   ██║██╔══██╗██╔══██║      *
+ *       ██║ ╚═╝ ██║╚██████╔╝██████╔╝███████╗██║  ██║██║  ██║██████╔╝╚██████╔╝██║  ██║██║  ██║      *
+ *       ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝      *
+ *                                                                                                  *
+ ****************************************************************************************************/
+
+var bib_requisicao  =   require("request");
+var base_url        =   "https://cleverbot.io/1.0/";
+var braunabot       =   function (user, key)
+{
+    this.user       =   user;
+    this.key        =   key;
+    this.setNick    =   function (nick)
+                        {
+                            this.nick = nick;
+                        }
+
+    this.create = function (callback)
+    {
+        bib_requisicao.post(
+        {
+            url: base_url + "create"
+           , form:
+           {
+                user: this.user,
+                key: this.key,
+                nick: this.nick
+            }
+        }, function (err, httpResponse, body)
+        {
+            if (err) throw err;
+            try {
+                var status = JSON.parse(body).status;
+            }
+            catch (e) {
+                console.log(e.stack);
+                var status="API endpoints unreachable";
+            }
+            if (status == "success") {
+                this.setNick(JSON.parse(body).nick);
+                callback(false, this.nick);
+            }
+            else if (status == "Error: reference name already exists") {
+                callback(false, this.nick);
+            }
+            else {
+                callback(status, null);
+            }
+        }.bind(this));
+    } // this.create = function (callback)
+
+    this.ask = function (input, callback)
+    {
+        request.post({ url: base_url + "ask", form: {
+                user: this.user,
+                key: this.key,
+                nick: this.nick,
+                text: input
+            }}, function (err, httpResponse, body) {
+                if (err) throw err;
+                if (JSON.parse(body).status == "success") {
+                    callback(false, JSON.parse(body).response);
+                }
+                else {
+                    callback(true, JSON.parse(body).status);
+                }
+            });
+    } // this.ask = function (input, callback)
+} // var braunabot       =   function (user, key)
+module.exports = braunabot;
