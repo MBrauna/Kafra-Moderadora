598083b93f77583448d50a0d3a19991c646c39d4
Michel Brauna <michelbrauna@gmail.com>
2018-03-06 19:58:44 -0300

    Update braunabot.js

--- _BraunaBot/braunabot.js
+++ _BraunaBot/braunabot.js
@@ -132,19 +132,7 @@ class braunabot
             ,(p_erro, p_resposta, p_corpo) =>
             {
                 v_obj_retorno   = JSON.parse(p_corpo);
-               if(v_obj_retorno.status === 'success')
-                {
-                    console.log(v_obj_retorno.response);
-                    return;
-                } // if(v_obj_retorno.status === 'success')
-                else
-                {
-                    p_mensagem.channel.send(
-                                                'Pessoa, tenho uma notícia ruim!'
-                                               ,v_obj_resposta
-                                            );
-                    return;
-                } // else{ ...}
+               console.log(v_obj_retorno);
             }); // request.post(
         } // try { ... }
         catch(p_erro)
