448e9f36a22643d3308c565b71c8d7cf879ea51f
Michel Brauna <michelbrauna@gmail.com>
2018-03-10 00:07:19 -0300

    DEBUG
    DEBUG

--- Kafra-Moderadora/kafra-moderadora.js
+++ Kafra-Moderadora/kafra-moderadora.js
@@ -120,7 +120,7 @@ class Kafra_moderadora
 
                                             try
                                             {
-                                                var v_canal     =   this.init_kafra_moderadora;
+                                                var v_canal     =   this.init_kafra_moderadora.server.defaultChannel;
                                                 console.log(v_canal);
                                             }
                                             catch(p_erro)
