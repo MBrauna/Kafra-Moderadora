50afef1000ecbcb02b5312d38e1f8f3b9bd14ca9
Michel Brauna <michelbrauna@gmail.com>
2018-03-13 06:30:53 -0300

    Arquivo SQL - Sequência
    Criação do arquivo SQL para armazenamento de sequências do banco de dados.

---
+++ database/sql/sequencia/kafra_moderadora.sql
@@ -0,0 +1,3 @@
+-- Criação de sequências para kafra-moderadora
+create sequence seq_aplicacao increment by 1 minvalue 1  maxvalue 999999 start 1;
+create sequence seq_aplicacao increment by 1 minvalue 1  maxvalue 999999 start 1;
