2121eda7b4705bae4288726333404391afc62a85
Michel Brauna <michelbrauna@gmail.com>
2018-03-19 00:10:40 -0300

    Armazenamento de log
    Armazenamento de log para Kafra Moderadora

---
+++ Provedor/gera_log.js
@@ -0,0 +1,79 @@
+/****************************************************************************************************
+ * Autor: Michel Brauna                                                            Data: 13/03/2018 *
+ *                                                                                                  *
+ ****************************************************************************************************
+ *                                                                                                  *
+ *                             ██╗  ██╗ █████╗ ███████╗██████╗  █████╗                              *
+ *                             ██║ ██╔╝██╔══██╗██╔════╝██╔══██╗██╔══██╗                             *
+ *                             █████╔╝ ███████║█████╗  ██████╔╝███████║                             *
+ *                             ██╔═██╗ ██╔══██║██╔══╝  ██╔══██╗██╔══██║                             *
+ *                             ██║  ██╗██║  ██║██║     ██║  ██║██║  ██║                             *
+ *                             ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝                             *
+ *                                                                                                  *
+ *       ███╗   ███╗ ██████╗ ██████╗ ███████╗██████╗  █████╗ ██████╗  ██████╗ ██████╗  █████╗       *
+ *       ████╗ ████║██╔═══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔═══██╗██╔══██╗██╔══██╗      *
+ *       ██╔████╔██║██║   ██║██║  ██║█████╗  ██████╔╝███████║██║  ██║██║   ██║██████╔╝███████║      *
+ *       ██║╚██╔╝██║██║   ██║██║  ██║██╔══╝  ██╔══██╗██╔══██║██║  ██║██║   ██║██╔══██╗██╔══██║      *
+ *       ██║ ╚═╝ ██║╚██████╔╝██████╔╝███████╗██║  ██║██║  ██║██████╔╝╚██████╔╝██║  ██║██║  ██║      *
+ *       ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝      *
+ *                                                                                                  *
+ ****************************************************************************************************
+ *                            Projeto Kafra Moderadora - Bot para discord                           *
+ ****************************************************************************************************/
+
+let   bib_requisicao        =   require('request')            // Para consulta a API de Ragnaplace
+     ;
+
+class log
+{
+
+    constructor(p_cliente)
+    {
+        this.obj_cliente    =   p_cliente;
+    } // constructor(p_cliente, p_mensagem)
+
+
+
+    log_mensagem(p_mensagem, p_mensagem_antiga)
+    {
+        let v_url_log           =   'http://kafra.mbrauna.org/api/log'
+           ,v_informacao        =   {
+                                        'token'     :   process.env.TOKEN_KAFRA_ADMIN
+                                       ,'cliente'   :   JSON.stringify(this.obj_cliente)
+                                       ,'evento'    :   'mensagem'
+                                       ,'nome_obj1' :   'mensagem_atual'
+                                       ,'nome_obj2' :   'mensagem_anterior'
+                                       ,'obj1'      :   JSON.stringify(p_mensagem)
+                                       ,'obj2'      :   JSON.stringify(p_mensagem_antiga)
+                                    }
+           ,v_arquivo_data      =   {
+                                        url     :   v_url_log
+                                       ,json    :   true
+                                       ,body    :   JSON.stringify(v_informacao);
+                                    }
+           ,v_informacao        =   {}
+            ;
+
+
+
+        request.post(v_arquivo_data, (p_erro, p_resposta, p_corpo) =>
+        {
+            v_informacao    =   JSON.parse(p_corpo);
+
+
+            if(typeof v_resposta === 'undefined' || v_resposta === 'null' || v_resposta === null || v_resposta === 'undefined')
+            {
+                return 9;
+            }
+
+            if(v_informacao.codigo === '1')
+            {
+                return 1;
+            }
+            else
+            {
+                return 9;
+            }
+        });
+    } // log_mensagem(p_mensagem, p_mensagem_atual)
+} // class log
