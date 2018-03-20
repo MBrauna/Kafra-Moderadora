f1c0547ecbe83fbdc6d97251a4335ec1b85535a9
Michel Brauna <michelbrauna@gmail.com>
2018-03-20 01:12:42 -0300

    Método de estatísticas
    Os dados de estatísticas serão criados para melhor aproveitamento dos eventos.
    Para todo evento iniciado uma trigger será lançada.

--- Provedor/comunica_mensagem.js
+++ Provedor/comunica_mensagem.js
@@ -21,60 +21,38 @@
  *                            Projeto Kafra Moderadora - Bot para discord                           *
  ****************************************************************************************************/
 
-let   bib_requisicao        =   require('request')            // Para consulta a API de Ragnaplace
+// Inicialização de bibliotecas                                 (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
+let   bib_requisicao        =   require('request')
      ;
+// Inicialização de bibliotecas                                 (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
 
-class log
+
+class comunica_msg
 {
-
     constructor(p_cliente)
     {
         this.obj_cliente    =   p_cliente;
-    } // constructor(p_cliente, p_mensagem)
+    } // constructor(p_cliente)
 
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
 
- 
-    log_mensagem(p_mensagem, p_mensagem_antiga, callback)
+    quantidade_palavra(p_conteudo)
     {
-        let v_url_log           =   'http://kafra.mbrauna.org/api/log'
-           ,v_informacao        =   {
-                                        'token'     :   process.env.TOKEN_KAFRA_ADMIN
-                                       ,'cliente'   :   this.obj_cliente
-                                       ,'evento'    :   'mensagem'
-                                       ,'nome_obj1' :   'mensagem_atual'
-                                       ,'nome_obj2' :   'mensagem_anterior'
-                                       ,'obj1'      :   p_mensagem
-                                       ,'obj2'      :   p_mensagem_antiga
-                                    }
-           ,v_arquivo_data      =   {
-                                        url     :   v_url_log
-                                       ,json    :   true
-                                       ,body    :   v_informacao
-                                    }
-           ;
+        // Quebra a frase/conteúdo em uma array - separado por espaço
+        var v_conteudo  =   p_conteudo.trim().split(/ +/g);
 
+        // Finaliza a função retornando a quantia de palavras
+        return v_conteudo.length; // Adiciona 1 ao valor final para eliminar a casa 0
+    } // quantidade_palavra(p_conteudo)
 
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
 
-        bib_requisicao.post(v_arquivo_data, (p_erro, p_resposta, p_corpo) =>
-        {
-            if(typeof p_corpo === 'undefined')
-            {
-                return callback(8);
-            }
+    quantidade_letra(p_conteudo)
+    {
+        // Mede o tamanho de caracteres em p_conteudo
+        return p_conteudo.length; // Adiciona 1 ao valor final para eliminar a casa 0
+    } // quantidade_letra(p_conteudo)
+} // class comunica_msg
 
-            if(p_corpo.codigo === '1')
-            {
-                return callback(1);
-            }
-            else
-            {
-                return callback(9);
-            }
-        });
-    } // log_mensagem(p_mensagem, p_mensagem_atual)
-
-
-} // class log
-
-
-module.exports = log;
+// Torna o método público - Acesso externo é permitido.
+module.exports = comunica_msg;
