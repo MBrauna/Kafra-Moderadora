142dda3611222447bad968a85c828dbff87889a4
Michel Brauna <michelbrauna@gmail.com>
2018-03-24 20:12:25 -0300

    Método Kafra Moderadora - Discord

--- kafra-moderadora.js
+++ kafra-moderadora.js
@@ -22,15 +22,14 @@
  ****************************************************************************************************/
 
 // Inicialização de bibliotecas                                 (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
-const bib_kafra_moderadora        =   require('./Monitor/kafra-moderadora.js');     // Inicializa a biblioteca de funcionalidades Kafra Moderadora
+const bib_kafra_moderadora        =   require('./evento.js');     // Inicializa a biblioteca de funcionalidades Kafra Moderadora
 // Inicialização de bibliotecas                                 (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
 
 // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
 
 // Coleta de variáveis de ambiente                              （ ^_^）o自  自o（^_^ ）
 let     v_token_discord             =   process.env.BOT_TOKEN_DISCORD
-       ,v_token_braunabot           =   process.env.BOT_TOKEN_BRAUNABOT
-       ,v_usuario_braunabot         =   process.env.BOT_USER_BRAUNABOT
+       ,v_token_kafra               =   process.env.TOKEN_KAFRA
        ;
 // Coleta de variáveis de ambiente                              （ ^_^）o自  自o（^_^ ）
 
@@ -38,7 +37,6 @@ let     v_token_discord             =   process.env.BOT_TOKEN_DISCORD
 
 // Método construtor - Inicialização do processo                (╬ ಠ益ಠ)
 const init_kafra_moderadora         =   new bib_kafra_moderadora(v_token_discord                // Token do bot - ver arquivo .env
-                                                                ,v_token_braunabot              // Token do chatbot BraunaBot - ver arquivo .env
-                                                                ,v_usuario_braunabot            // Usuário do chatbot BraunaBot - ver arquivo .env
+                                                                ,v_token_kafra                  // Token de acesso para Kafra Moderadora.
                                                                 ); // new bib_kafra_moderadora
 // Método construtor - Inicialização do processo                (╬ ಠ益ಠ)
