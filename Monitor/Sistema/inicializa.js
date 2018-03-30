17bc312219b9123d62ce4624512baf2df9634172
Michel Brauna <michelbrauna@gmail.com>
2018-03-30 15:06:00 -0300

    Retornando ao método anterior
    JSON STRINGIFY NÃO É NECESSÁRIO.

--- Monitor/Sistema/inicializa.js
+++ Monitor/Sistema/inicializa.js
@@ -117,9 +117,9 @@ class inicializa
 
             // Monta o corpo da requisição, à fim de definir os dados à serem tratados
             v_corpo         =   {
-                                    'Usuario'   :   JSON.stringify(obj_usuario)
-                                   ,'Servidor'  :   JSON.stringify(obj_servidor)
-                                   ,'Origem'    :   JSON.stringify(v_origem)
+                                    'Usuario'   :   obj_usuario
+                                   ,'Servidor'  :   obj_servidor
+                                   ,'Origem'    :   v_origem
                                 };
 
             // Monta a requisição para o serviço desejado
