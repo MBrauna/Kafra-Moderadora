2be20fe770d994d55c479c7050770829c59c84ae
Michel Brauna <michelbrauna@gmail.com>
2018-04-03 23:09:47 -0300

    API - MESSAGE- Tratativa
    Pacote de tratativas para o evento - MESSAGE - a API irá verificar e gerar estatísticas das respostas.

--- Monitor/Usuario/mensagem.js
+++ Monitor/Usuario/mensagem.js
@@ -49,6 +49,68 @@ class mensagem
 
     // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ
 
+    /***********************************
+     * Métodos internos para mensagens *
+     ***********************************/
+    mensagem_para_array()
+    {
+        /***************************************************
+         * Autor: Michel Brauna           Data: 16/03/2018 *
+         *   Método irá obter a frase gerada e tratá-la de *
+         * forma à transformá-la numa array.               *
+         ***************************************************/
+        return this.obj_mensagem.content.trim().split(/ +/g);
+    } // mensagem_para_array() { ... }
+
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
+
+    mensagem_para_array_sprefixo(p_prefixo)
+    {
+        /***************************************************
+         * Autor: Michel Brauna           Data: 16/03/2018 *
+         *   Método irá obter a frase gerada e tratá-la de *
+         * forma à transformá-la numa array sem o prefixo  *
+         ***************************************************/
+        var v_tamanho_mencao            =   p_prefixo.length;
+
+        // Verifica se ocorreu menção ao bot
+        if(this.verifica_mencao())
+        {
+            // Caso tenha ocorrido ... retorna a array sem prefixo
+            return this.obj_mensagem.content.slice(v_tamanho_mencao).trim().split(/ +/g);
+        } // if(this.verifica_mencao()) { ... }
+        else
+        {
+            // Caso contrário ... nada.
+            return [];
+        } // else { ... }
+
+    } //  mensagem_para_array_sprefixo(p_prefixo) { ... }
+
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
+
+    mencao_bot()
+    {
+        /***************************************************
+         * Autor: Michel Brauna           Data: 16/03/2018 *
+         *Retorno: menção direta ao bot.                   *
+         ***************************************************/
+        return '<@' + this.obj_cliente.user.id + '>';
+    } // mencao_bot() { ... }
+
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
+
+    mencao_autor()
+    {
+        /***************************************************
+         * Autor: Michel Brauna           Data: 16/03/2018 *
+         *Retorno: menção direta ao autor da mensagem.     *
+         ***************************************************/
+        return '<@' + this.obj_mensagem.author.id + '>';
+    } // mencao_autor() { ... }
+
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ
+
     verifica_bot()
     {
         /***************************************************
@@ -57,6 +119,13 @@ class mensagem
          ***************************************************/
         return this.obj_mensagem.author.bot;
     } // verifica_bot() { ... }
+
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ
+
+    verifica_mencao()
+    {
+        return  this.obj_mensagem.content.startsWith(this.mencao_bot());
+    } // verifica_mencao() { ... }
 
     // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
 
@@ -112,11 +181,15 @@ class mensagem
 
             // Inicia a criação do corpo
             v_obj_requisicao        =   {
-                                            autor       :   this.obj_mensagem.author
-                                           ,tts         :   this.obj_mensagem.tts
-                                           ,id          :   this.obj_mensagem.id
-                                           ,conteudo    :   this.obj_mensagem.content
-                                           ,mencao      :   this.obj_mensagem.mentions.users
+                                            autor               :   this.obj_mensagem.author
+                                           ,bot                 :   this.verifica_bot()
+                                           ,canal               :   this.obj_mensagem.channel
+                                           ,tts                 :   this.obj_mensagem.tts
+                                           ,id                  :   this.obj_mensagem.id
+                                           ,conteudo            :   this.obj_mensagem.content
+                                           ,conteudo_array      :   this.mensagem_para_array()
+                                           ,usa_comando         :   this.verifica_mencao()
+                                           ,conteudo_comando    :   this.mensagem_para_array_sprefixo(this.mencao_bot())
                                         };
 
             v_obj_requisicao        =   {
