dab52110d40b9919608548f7b38ded76674adc60
Michel Brauna <michelbrauna@gmail.com>
2018-04-16 14:20:21 -0300

    Update Comando.js

--- Kafra-Moderadora/Comando.js
+++ Kafra-Moderadora/Comando.js
@@ -20,8 +20,10 @@
 
 // Inicialização de bibliotecas                                 (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
 let  bib_bropedia       =   require('./Pesquisa/bropedia/bropedia.js')
-     bib_ragnaplace     =   require('./Pesquisa/ragnaplace/ragnaplace.js')
-     bib_grupo          =   require('./Ragnarok/grupo_ragnarok.js')
+    ,bib_ragnaplace     =   require('./Pesquisa/ragnaplace/ragnaplace.js')
+    ,bib_grupo          =   require('./Ragnarok/grupo_ragnarok.js')
+    ,bib_requisicao     =   require('request')
+    ,bib_url            =   process.env.url_requisicao
     ;
 // Inicialização de bibliotecas                                 (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
 
@@ -39,6 +41,36 @@ class comando
     } // Método construtor - construct()
 
     // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ
+     
+    comunica_portal(p_dados_url, callback)
+    {
+        /**********************************************************
+         * Autor: Michel Brauna                  Data: 27/03/2018 *
+         *    Processo para comunicar com o servidor Kafra.       *
+         **********************************************************/
+
+        try
+        {
+            bib_requisicao.post(p_dados_url, (p_erro, p_resposta, p_corpo) =>
+            {
+                let v_req_erro       =   p_erro
+                   ,v_req_resposta   =   p_resposta
+                   ,v_req_corpo      =   p_corpo
+                   ;
+
+                // Retorna para a função callback;
+                callback(v_req_erro, v_req_resposta, v_req_corpo);
+            }); // bib_requisicao.post(p_dados_url, (p_erro, p_resposta, p_corpo) =>
+        } // try { ... }
+        catch(p_erro)
+        {
+            console.log('--- ERRO - COMUNICA_PORTAL ---');
+            console.trace();
+            console.log(p_erro);
+        } // catch(p_erro) { ... }
+    } // comunica_portal(p_dados_url, callback)
+     
+     // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ
 
     // Procedimento responsável peló retorno da mensagem em seu formato esperado
     monta_resposta(p_cliente, p_mensagem, p_frase, p_configuracao)
@@ -68,6 +100,70 @@ class comando
 
     // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ
 
+    mensagem_para_array(p_mensagem)
+    {
+        /***************************************************
+         * Autor: Michel Brauna           Data: 16/03/2018 *
+         *   Método irá obter a frase gerada e tratá-la de *
+         * forma à transformá-la numa array.               *
+         ***************************************************/
+        return p_mensagem.content.trim().split(/ +/g);
+    } // mensagem_para_array() { ... }
+
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ 
+
+    mensagem_para_array_sprefixo(p_prefixo, p_mensagem)
+    {
+        /***************************************************
+         * Autor: Michel Brauna           Data: 16/03/2018 *
+         *   Método irá obter a frase gerada e tratá-la de *
+         * forma à transformá-la numa array sem o prefixo  *
+         ***************************************************/
+        var v_tamanho_mencao            =   p_prefixo.length;
+
+        // Verifica se ocorreu menção ao bot
+        if(this.verifica_mencao())
+        {
+            // Caso tenha ocorrido ... retorna a array sem prefixo
+            return p_mensagem.content.slice(v_tamanho_mencao).trim().split(/ +/g);
+        } // if(this.verifica_mencao()) { ... }
+        else
+        {
+            // Caso contrário ... nada.
+            return [];
+        } // else { ... }
+
+    } //  mensagem_para_array_sprefixo(p_prefixo) { ... }
+
+    mencao_bot(p_cliente)
+    {
+        /***************************************************
+         * Autor: Michel Brauna           Data: 16/03/2018 *
+         *Retorno: menção direta ao bot.                   *
+         ***************************************************/
+        return '<@' + p_cliente.user.id + '>';
+    } // mencao_bot() { ... }
+
+
+    verifica_bot(p_mensagem)
+    {
+        /***************************************************
+         * Autor: Michel Brauna           Data: 16/03/2018 *
+         * Retornará se o usuário é bot ou não             *
+         ***************************************************/
+        return p_mensagem.author.bot;
+    } // verifica_bot() { ... }
+
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ
+
+    verifica_mencao(p_mensagem)
+    {
+        return  p_mensagem.content.startsWith(this.mencao_bot());
+    } // verifica_mencao() { ... }
+
+
+    // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ
+
     trata_mensagem(p_cliente, p_mensagem)
     {
         // Declaração de variáveis 
@@ -75,6 +171,8 @@ class comando
             ,v_str_mencao_usuario       =   '<@' + p_mensagem.author.id + '>'
             ,v_bol_chamada              =   p_mensagem.content.startsWith(v_str_mencao_kafra)
             ,v_obj_mensagem_s_prefixo   =   [] // p_mensagem.content.slice(v_str_mencao_kafra.length).trim().split(/ +/g)
+            v_obj_corpo                 =   {}
+           ,v_obj_requisicao            =   {}
             ,v_string_requisicao
             ;
 
@@ -85,6 +183,43 @@ class comando
             if(p_mensagem.author.bot) return; // Caso seja de um bot finaliza a verificação
 
             // ᕦ(ò_óˇ)ᕤ     ---     S E P A R A D O R     ---     ᕦ(ˇò_ó)ᕤ
+            
+            try
+            {
+                // Inicia a criação do corpo
+                v_obj_requisicao        =   {
+                                                autor               :   p_mensagem.author
+                                               ,bot                 :   this.verifica_bot(p_mensagem)
+                                               ,canal               :   p_mensagem.channel
+                                               ,tts                 :   p_mensagem.tts
+                                               ,id                  :   p_mensagem.id
+                                               ,conteudo            :   p_mensagem.content
+                                               ,conteudo_array      :   this.mensagem_para_array(p_mensagem)
+                                               ,usa_comando         :   this.verifica_mencao(p_mensagem)
+                                               ,conteudo_comando    :   this.mensagem_para_array_sprefixo(this.mencao_bot(p_cliente),p_mensagem)
+                                            };
+
+                v_obj_requisicao        =   {
+                                                'url'           :   bib_url + 'mensagem'
+                                               ,'json'          :   true
+                                               ,'body'          :   v_obj_requisicao
+                                               ,'headers'       :   {
+                                                                        'Authorization'     :   process.env.TOKEN_AUTORIZACAO
+                                                                    }
+                                            };
+
+                // Comunica com o servidor
+                this.comunica_portal(v_obj_requisicao, (p_erro, p_resposta, p_corpo) =>
+                {
+                    console.log(p_corpo);
+                }); // this.comunica_portal(v_obj_requisicao, (p_erro, p_resposta, p_corpo) =>
+            } // try { ... }
+            catch($p_erro)
+            {
+                console.log('Não foi possível salvar! Informações');
+            } // catch($p_erro) { ... }
+           
+            
 
             // Verifica se a mensagem foi iniciada com uma menção à Kafra Moderadora
             if(v_bol_chamada)
